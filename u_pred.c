990fb53a37541440fe98dd740db86193349dfcf9
DmANikulin <dmanikulin@mai.education>
2025-12-25 18:00:53 +0300

    last

--- u_pred.c
+++ u_pred.c
@@ -13,12 +13,3 @@
       \forall integer i; 0 <= i < 50 ==> \valid(req->headers[i] + (0..255));
 */
 
-/*@
-  requires valid_req_struct(req);
-  requires 0 <= req->header_count <= 50;
-  // Требование: все headers[i] терминированы
-  requires \forall integer i; 0 <= i < req->header_count ==> cstrn_valid(req->headers[i],256);
-
-  assigns \nothing;
-*/
-void process_http_headers(HttpRequest* req);
